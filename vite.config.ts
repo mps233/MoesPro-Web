ba0ee70037e8efe7e737ac991c0cc11140fb39ad
jxxghp <jxxghp@gmail.com>
2025-07-14 11:11:42 +0800

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,21 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      output: {
+        // 固定 JS 文件名
+        entryFileNames: 'js/[name].js',
+        chunkFileNames: 'js/[name].js',
+        assetFileNames: (assetInfo) => {
+          // 固定 CSS 文件名
+          if (assetInfo.name && assetInfo.name.endsWith('.css')) {
+            return 'css/[name].css'
+          }
+          // 其他资源文件
+          return 'assets/[name].[ext]'
+        }
+      }
+    }
+  }
 })
