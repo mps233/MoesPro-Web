20cde6249b9d1b936b88b11e2238cda5300cb003
mps233 <zyj19961209@gmail.com>
2025-08-04 03:17:39 +0800

    添加pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,33 @@
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Moes Pro',
+        short_name: 'MoesPro',
+        description: '智能观影系统',
+        theme_color: '#1e293b',
+        icons: [
+          {
+            src: 'icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   build: {
     rollupOptions: {
       output: {
