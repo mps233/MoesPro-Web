ef603e5aea288ca7600dce115addcce2ee8b7753
mps233 <zyj19961209@gmail.com>
2025-08-04 03:28:50 +0800

    添加pwa

--- vite.config.ts
+++ vite.config.ts
@@ -15,12 +15,12 @@ export default defineConfig({
         theme_color: '#1e293b',
         icons: [
           {
-            src: 'assets/logo-192.png',
+            src: 'icon-192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'assets/logo-512.png',
+            src: 'icon-512.png',
             sizes: '512x512',
             type: 'image/png'
           }
